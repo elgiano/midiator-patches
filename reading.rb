192d55aca036dbf5a87dae40855fa61b5c4cbe28
giano <giano@labmotus.(none)>
2010-08-27 13:01:22 +0200

    missing code added

--- reading.rb
+++ reading.rb
@@ -10,6 +10,7 @@
 # This code released under the terms of the MIT license.
 
 require 'midiator'
+require 'dl/import'
 
 class MIDIator::Interface
   attr_reader :read_data
@@ -25,24 +26,37 @@ class MIDIator::Interface
         at_exit{Process.kill "KILL",cpid}
         loop do
         IO.select([rd])
-        @read_data = rd.gets.unpack("CCC")
-        callback.call
+        @read_data = rd.gets
+        next if not @read_data
+        @read_data = @read_data.gsub(/\\n/,"\n").unpack("CCC") # unescape \\n
+        callback.call @read_data
         end
       else
         rd.close
-        loop do wr.puts read end
+        loop do wr.puts read.gsub(/\n/,"\\\\n") end # escape \n
       end
       }
     end
   end
   # Read MIDI inputs. Implemented only for ALSA drivers
   def read
-    @bytes = @driver.read.to_s(3)
-    return @bytes.to_s#.unpack("CCC")
+    bytes = @driver.read
+    return bytes.to_s(3)
   end
 end
 
 class MIDIator::Driver::ALSA < MIDIator::Driver
+  module C
+    case RUBY_VERSION.to_f
+       when 1.8
+        extend DL::Importable
+       when 1.9
+        extend DL::Importer
+    end
+    dlload 'libasound.so'
+    extern "int snd_rawmidi_read(void*, void*, int)"
+  end
+  
   def read()
     bytes = [999,999,999].pack("CCC").to_ptr
     C.snd_rawmidi_read(@input,bytes,3)
